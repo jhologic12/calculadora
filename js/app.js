0ba1373832efaf9e6dcce7f1289d741ac6192a96
jhologic12 <jhonospino@gmail.com>
2022-11-06 18:50:07 -0500

    Agregando funcionalidades para habilitar operacion

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log("la aplicacion es iniciada");
+
+
+function sumar (){
+
+    const formul = document.getElementById('formul');
+
+    let operandoA = formul['operandoA'];
+    let operandoB = formul['operandoB'];
+
+    let resultado= parseInt(operandoA.value) + parseInt(operandoB.value);
+
+    if(isNaN(resultado))
+        resultado = 'la operacion requiere valores numéricos';
+
+
+document.getElementById('resultado').innerHTML = `El Resultado es: ${resultado}`;
+}
